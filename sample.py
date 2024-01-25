2372876f653b1480fd2cedc81a0065754cacccf1
jaeyung1001 <jaeyung1001@naver.com>
2024-01-25 17:58:07 +0900

    remove

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import notifier
 if __name__ == '__main__':
-    my_token = "xoxb-1394758654305-3810545912694-xIEY2rQfJ6imHAP0kHhOierb"
+    my_token = ""
     channel_id = "jaeyung_channel"
     slack = notifier.SlackNotifier(my_token)
     slack.send_message(channel_id, "test from jaeyung pc")
