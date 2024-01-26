6919be61c930de5751223764fc4fdc1df7b1f79b
jaeyung2 <jaeyung2@moreh.io>
2024-01-26 17:10:33 +0900

    test

--- sample.py
+++ sample.py
@@ -2,6 +2,6 @@ import notifier
 
 if __name__ == '__main__':
     my_token = ""
-    channel_id = ""
+    channel_id = "test"
     slack = notifier.SlackNotifier(my_token)
     slack.send_message(channel_id, "test from jaeyung pc")
